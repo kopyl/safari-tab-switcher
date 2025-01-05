bd0d3394fe44d27d73bcc01cfc4d07390b41aaa9
kopyl <oleggggg1997@gmail.com>
2025-01-05 11:50:56 +0200

    Refactor

--- tab_finder/ViewController.swift
+++ tab_finder/ViewController.swift
@@ -1,13 +1,1 @@
-import Cocoa
-import SafariServices
-import SwiftUI
 
-@available(macOS 10.15, *)
-struct HelloWorldView: View {
-    var body: some View {
-        VStack {
-            Text("Hello, World!")
-            .padding()
-        }
-    }
-}
