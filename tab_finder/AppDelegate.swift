eee943d909c6b2e33e97fd5d7cd4b508584a02ab
kopyl <oleggggg1997@gmail.com>
2025-01-05 11:22:31 +0200

    Add SwiftUI view to replace later storyboard with

--- tab_finder/AppDelegate.swift
+++ tab_finder/AppDelegate.swift
@@ -1,13 +1,44 @@
 import Cocoa
+import SwiftUI
 
+@available(macOS 10.15, *)
+struct HelloWorldView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .padding()
+            Button("Close") {
+            }
+            .padding()
+        }
+    }
+}
+
+@available(macOS 10.15, *)
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var window: NSWindow!
+
     func applicationDidFinishLaunching(_ notification: Notification) {
+        // Create the SwiftUI view
+        let contentView = HelloWorldView()
+
+        // Create the window and set the content view
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
+            styleMask: [.titled, .closable, .resizable, .miniaturizable],
+            backing: .buffered,
+            defer: false
+        )
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
-}
+}	
