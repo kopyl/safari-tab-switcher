305b585a3bd41ead0562bc94c5253c7ff53ab1cc
kopyl <oleggggg1997@gmail.com>
2025-01-05 10:41:29 +0200

    Add functions to send notifications and go to URLs

--- tab_finder Extension/SafariExtensionHandler.swift
+++ tab_finder Extension/SafariExtensionHandler.swift
@@ -1,6 +1,7 @@
 import SafariServices
 import os.log
 import SwiftUI
+import UserNotifications
 
 @available(macOSApplicationExtension 10.15, *)
 struct HelloWorldView: View {
@@ -10,11 +11,42 @@ struct HelloWorldView: View {
                 .font(.largeTitle)
                 .padding()
             Button("Close") {
-                // Add functionality to close the popover if needed
             }
             .padding()
         }
     }
+}
+
+func sendNotification(subtitle: String) {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+        if success {
+            print("Permission approved!")
+        } else if let error = error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    let content = UNMutableNotificationContent()
+    content.title = "Notification"
+    content.subtitle = subtitle
+    content.sound = UNNotificationSound.default
+
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+    UNUserNotificationCenter.current().add(request)
+}
+
+func getAllTabsFromWindow(window: SFSafariWindow) {
+    window.getAllTabs(completionHandler: { allTabs in
+        print(allTabs)
+    })
+}
+
+func navigateToUrl(window: SFSafariWindow, url: String) {
+    window.getActiveTab(completionHandler: { (tab) in
+        if let myUrl = URL(string: url){
+            tab?.navigate(to: myUrl)
+        }
+    })
 }
 
 
@@ -23,7 +55,6 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
 
     override func toolbarItemClicked(in window: SFSafariWindow) {
         os_log(.default, "Toolbar item clicked")
-        // Safari will automatically show the popover via the popoverViewController() method
     }
 
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping (Bool, String) -> Void) {
