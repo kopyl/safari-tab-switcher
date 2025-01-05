152d7d62a9bfb4b241b4717c4e7df4582b657431
kopyl <oleggggg1997@gmail.com>
2025-01-05 11:45:16 +0200

    Remove storyboard in the main app
    in favor of SwiftUI

--- tab_finder/AppDelegate.swift
+++ tab_finder/AppDelegate.swift
@@ -1,30 +1,11 @@
-import Cocoa
 import SwiftUI
 
-@available(macOS 10.15, *)
+@available(macOS 11.0, *)
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    var window: NSWindow!
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // Create the SwiftUI view
-        let contentView = HelloWorldView()
-
-        // Create the window and set the content view
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 800, height: 600),
-            styleMask: [.titled, .closable, .resizable, .miniaturizable],
-            backing: .buffered,
-            defer: false
-        )
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+struct MySafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HelloWorldView()
+        }
     }
-
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        return true
-    }
-}	
+}
--- tab_finder/ViewController.swift
+++ tab_finder/ViewController.swift
@@ -1,53 +1,13 @@
 import Cocoa
 import SafariServices
 import SwiftUI
-import WebKit
-
-let extensionBundleIdentifier = "kopyl.tab-finder.Extension"
-
-@available(macOS 10.15, *)
-struct WebView: NSViewRepresentable {
-    let url: URL
-
-    func makeNSView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.navigationDelegate = context.coordinator
-        let request = URLRequest(url: url)
-        webView.load(request)
-        return webView
-    }
-
-    func updateNSView(_ nsView: WKWebView, context: Context) {
-        // Update the view if needed
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
-    }
-
-    class Coordinator: NSObject, WKNavigationDelegate {}
-}
 
 @available(macOS 10.15, *)
 struct HelloWorldView: View {
     var body: some View {
         VStack {
             Text("Hello, World!")
-                .font(.largeTitle)
-                .padding()
-
-            Button("Open Preferences") {
-                SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-                    DispatchQueue.main.async {
-                        NSApplication.shared.terminate(nil)
-                    }
-                }
-            }
             .padding()
-
-            WebView(url: Bundle.main.url(forResource: "Main", withExtension: "html")!)
-                .frame(height: 300)
         }
-        .padding()
     }
 }
