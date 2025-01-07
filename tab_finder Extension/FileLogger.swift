3f9d8e9b62d44617b530f3d023fead7e7c94a6ad
kopyl <oleggggg1997@gmail.com>
2025-01-07 05:32:47 +0200

    Implement file logger

---
+++ tab_finder Extension/FileLogger.swift
@@ -0,0 +1,37 @@
+import Foundation
+import os.log
+
+@available(macOSApplicationExtension 11.0, *)
+class FileLogger {
+    static let shared = FileLogger()
+    private let logFileURL: URL
+
+    private init() {
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        logFileURL = documentsURL.appendingPathComponent("SafariExtensionLogs.txt")
+
+        if !fileManager.fileExists(atPath: logFileURL.path) {
+            fileManager.createFile(atPath: logFileURL.path, contents: nil, attributes: nil)
+        }
+    }
+
+    func log(_ message: String) {
+        let timestamp = Date().description(with: .current)
+        let logMessage = "[\(timestamp)] \(message)\n"
+        os_log("Message: %{public}@", logMessage)
+        appendToFile(logMessage)
+    }
+
+    private func appendToFile(_ text: String) {
+        if let data = text.data(using: .utf8) {
+            if let fileHandle = try? FileHandle(forWritingTo: logFileURL) {
+                fileHandle.seekToEndOfFile()
+                fileHandle.write(data)
+                fileHandle.closeFile()
+            } else {
+                try? text.write(to: logFileURL, atomically: true, encoding: .utf8)
+            }
+        }
+    }
+}
