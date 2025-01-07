95d24c00cef13e4bbecb8515f1f799ed43a41716
kopyl <oleggggg1997@gmail.com>
2025-01-08 01:09:07 +0200

    Remove init command and add logging
    In the injected JS file

--- tab_finder Extension/Resources/script.js
+++ tab_finder Extension/Resources/script.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-});
-
 document.addEventListener("keydown", function (event) {
     if (event.altKey && event.key === "Tab") {
         event.preventDefault();
         safari.extension.dispatchMessage("Option+Tab Pressed");
+        console.log("Option+Tab Pressed")
     }
 });
+
