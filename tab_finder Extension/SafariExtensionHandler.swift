24a7d393ca50c664b4d4ea26d3657144745ecdf7
kopyl <oleggggg1997@gmail.com>
2025-01-07 22:43:48 +0200

    Remove logging

--- tab_finder Extension/SafariExtensionHandler.swift
+++ tab_finder Extension/SafariExtensionHandler.swift
@@ -44,7 +44,6 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
                         return
                     }
                     UserDefaults.standard.set(changedToTabIndex, forKey: "currentTabId")
-                    FileLogger.shared.log("Tab changed to id \(changedToTabIndex)")
                 }
             }
         }
