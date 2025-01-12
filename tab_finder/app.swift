95479e5e98b9454c08245decefaa34e62b5e92de
kopyl <oleggggg1997@gmail.com>
2025-01-12 05:24:46 +0200

    Implement showing app's window w/t menu bar

--- tab_finder/app.swift
+++ tab_finder/app.swift
@@ -1,11 +1,39 @@
-import SafariServices
 import SwiftUI
 
 struct HelloWorldView: View {
+    @State private var eventMonitor: Any?
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
             .padding()
+        }
+        .onAppear {
+            setupKeyListener()
+        }
+        .onDisappear {
+            removeKeyListener()
+        }
+    }
+    
+    private func bringWindowToFront() {
+        if let window = NSApplication.shared.windows.first {
+            window.orderFrontRegardless()
+        }
+    }
+    
+    func setupKeyListener() {
+        eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.keyDown]) { event in
+            if event.keyCode == 0x00 {
+                bringWindowToFront()
+            }
+        }
+    }
+    
+    func removeKeyListener() {
+        if let monitor = eventMonitor {
+            NSEvent.removeMonitor(monitor)
+            eventMonitor = nil
         }
     }
 }
