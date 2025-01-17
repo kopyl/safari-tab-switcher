453fc771321fef0669e8e2c8494f88de67770523
kopyl <oleggggg1997@gmail.com>
2025-01-17 16:25:06 +0200

    Remove titles and hosts from UserDefaults

--- tab_finder Extension/StoreManager.swift
+++ tab_finder Extension/StoreManager.swift
@@ -42,23 +42,6 @@ func decode<T: Codable>(_ type: T.Type, from data: Data) -> T? {
 struct Store {
     private static let userDefaults = UserDefaults(suiteName: "com.tabfinder.sharedgroup") ?? UserDefaults.standard
 
-    static var tabsTitleAndHost: TabsStorage {
-            get {
-                guard let data = userDefaults.data(forKey: "tabsTitleAndHost") else {
-                    return [:]
-                }
-
-                let decoder = JSONDecoder()
-                return (try? decoder.decode(TabsStorage.self, from: data)) ?? [:]
-            }
-            set {
-                let encoder = JSONEncoder()
-                if let data = try? encoder.encode(newValue) {
-                    userDefaults.set(data, forKey: "tabsTitleAndHost")
-                }
-            }
-        }
-
     static var tabIDsWithTitleAndHost: OrderedSet2<TabInfoWithID> {
             get {
                 if let data = userDefaults.data(forKey: "tabIDsWithTitleAndHost") {
