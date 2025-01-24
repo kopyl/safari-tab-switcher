8d147197f930078d9fd8b04080ab7297207a091f
kopyl <oleggggg1997@gmail.com>
2025-01-24 04:04:17 +0200

    Refactor

--- tab_finder Extension/StoreManager.swift
+++ tab_finder Extension/StoreManager.swift
@@ -33,16 +33,17 @@ func decode<T: Codable>(_ type: T.Type, from data: Data) -> T? {
 
 struct Store {
     private static let userDefaults = UserDefaults(suiteName: "com.tabfinder.sharedgroup") ?? UserDefaults.standard
+    private static let key = "tabIDsWithTitleAndHost"
 
     static var tabIDsWithTitleAndHost: Tabs {
             get {
-                guard let data = userDefaults.data(forKey: "tabIDsWithTitleAndHost") else { return Tabs() }
+                guard let data = userDefaults.data(forKey: key) else { return Tabs() }
                 guard let decodedData = decode([TabInfoWithID].self, from: data) else { return Tabs() }
                 return Tabs(decodedData)
             }
             set {
                 guard let encodedData = encode(Tabs(newValue.tabs).tabs) else { return }
-                userDefaults.set(encodedData, forKey: "tabIDsWithTitleAndHost")
+                userDefaults.set(encodedData, forKey: key)
             }
         }
 }
