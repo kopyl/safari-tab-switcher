6d18c26e60418c4a48d19d72b48f665ddfa2e049
kopyl <oleggggg1997@gmail.com>
2025-02-22 07:02:31 +0200

    Remove unused package's files

--- Packages/HotKey/Example/Sources/AppDelegate.swift
+++
@@ -1,3 +0,0 @@
-import AppKit
-
-@NSApplicationMain final class AppDelegate: NSObject, NSApplicationDelegate {}
--- Packages/HotKey/Support/HotKey.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for HotKey.
-FOUNDATION_EXPORT double HotKeyVersionNumber;
-
-//! Project version string for HotKey.
-FOUNDATION_EXPORT const unsigned char HotKeyVersionString[];
