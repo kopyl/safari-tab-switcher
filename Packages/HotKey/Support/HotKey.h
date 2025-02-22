a696a007980f29d3c8330940fa320983e6f777b3
kopyl <oleggggg1997@gmail.com>
2025-02-22 04:28:22 +0200

    Put the HotKey dependency inside the project
    Because i want to at least add logging easily

---
+++ Packages/HotKey/Support/HotKey.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for HotKey.
+FOUNDATION_EXPORT double HotKeyVersionNumber;
+
+//! Project version string for HotKey.
+FOUNDATION_EXPORT const unsigned char HotKeyVersionString[];
