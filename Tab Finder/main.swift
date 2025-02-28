a280368b1938233d1d7229f137a07b14f59e7d2d
kopyl <oleggggg1997@gmail.com>
2025-02-28 16:40:50 +0200

    Move to custom NSApplication
    To have more flexibility over the app

---
+++ Tab Finder/main.swift
@@ -0,0 +1,132 @@
+import Foundation
+import HotKey
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+    var showOrHideTabsHistoryWindowHotKey: HotKey?
+    var appState: AppState?
+    private var activeAppObserver: Any?
+    
+    init(
+        showOrHideTabsHistoryWindowHotKey: HotKey,
+        appState: AppState
+    ) {
+        self.showOrHideTabsHistoryWindowHotKey = showOrHideTabsHistoryWindowHotKey
+        self.appState = appState
+    }
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let hotKey = showOrHideTabsHistoryWindowHotKey, let state = appState {
+            showMainWindow(showOrHideTabsHistoryWindowHotKey: hotKey, appState: state)
+        }
+        
+        setupAppSwitchingObserver()
+        setUpNSWindowDelegate()
+    }
+    
+    func setUpNSWindowDelegate() {
+        greetingWindow?.delegate = self
+    }
+    
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        NSApp.setActivationPolicy(.accessory)
+        NSApp.hide(nil)
+        return false
+    }
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        showGreetingWindow(appState: appState)
+        hideMainWindow()
+        return true
+    }
+    
+    func application(_ application: NSApplication, open urls: [URL]) {
+            for url in urls {
+                if url.scheme == "tabfinder" {
+                    showGreetingWindow(appState: appState)
+                }
+            }
+        }
+    
+    func setupAppSwitchingObserver() {
+        let workspace = NSWorkspace.shared
+        let notificationCenter = workspace.notificationCenter
+        
+        activeAppObserver = notificationCenter.addObserver(
+            forName: NSWorkspace.didActivateApplicationNotification,
+            object: nil,
+            queue: .main
+        ) { notification in
+            guard let app = notification.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication else {
+                return
+            }
+
+            if let bundleIdentifier = app.bundleIdentifier {
+                if bundleIdentifier == "com.apple.Safari" {
+                    self.showOrHideTabsHistoryWindowHotKey?.isPaused = false
+                } else {
+                    self.showOrHideTabsHistoryWindowHotKey?.isPaused = true
+                }
+            }
+        }
+    }
+}
+
+class CustomApplication: NSApplication {
+    private func createMenu() {
+        let mainMenu = NSMenu()
+        
+        let appMenuItem = NSMenuItem()
+        mainMenu.addItem(appMenuItem)
+        
+        let appMenu = NSMenu()
+        appMenuItem.submenu = appMenu
+        
+
+        let aboutMenuItem = NSMenuItem(title: "About \(ProcessInfo.processInfo.processName)",
+                                      action: #selector(orderFrontStandardAboutPanel(_:)),
+                                      keyEquivalent: "")
+        appMenu.addItem(aboutMenuItem)
+        
+        appMenu.addItem(NSMenuItem.separator())
+        
+        let quitMenuItem = NSMenuItem(title: "Quit \(ProcessInfo.processInfo.processName)",
+                                     action: #selector(terminate(_:)),
+                                     keyEquivalent: "q")
+        appMenu.addItem(quitMenuItem)
+        
+        let windowMenuItem = NSMenuItem()
+        mainMenu.addItem(windowMenuItem)
+        
+        let windowMenu = NSMenu(title: "Window")
+        windowMenuItem.submenu = windowMenu
+        
+        let closeMenuItem = NSMenuItem(title: "Close Window",
+           action: #selector(NSWindow.performClose(_:)),
+           keyEquivalent: "w")
+        
+        windowMenu.addItem(closeMenuItem)
+        
+        self.mainMenu = mainMenu
+    }
+    
+    override init() {
+        super.init()
+        
+        createMenu()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+let delegate = AppDelegate(
+    showOrHideTabsHistoryWindowHotKey: HotKey(key: .tab, modifiers: [.option]),
+    appState: AppState()
+)
+
+let app = CustomApplication.shared
+app.delegate = delegate
+
+app.run()
