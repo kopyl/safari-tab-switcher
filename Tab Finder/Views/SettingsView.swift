d1ce1f4835a4195af86464c859c334b33aa40981
kopyl <oleggggg1997@gmail.com>
2025-03-11 17:03:45 +0200

    Improve copy
    Tell a user on exactly which key modifier release the tabs list window is going to be hidden

--- Tab Finder/Views/SettingsView.swift
+++ Tab Finder/Views/SettingsView.swift
@@ -13,17 +13,45 @@ prefix func ! (value: Binding<Bool>) -> Binding<Bool> {
     )
 }
 
+extension NSEvent.ModifierFlags {
+    var symbolRepresentation: String {
+        var symbols = ""
+
+        if contains(.command) {
+            symbols += "⌘"
+        }
+        if contains(.option) {
+            symbols += "⌥"
+        }
+        if contains(.control) {
+            symbols += "⌃"
+        }
+        if contains(.shift) {
+            symbols += "⇧"
+        }
+        if contains(.capsLock) {
+            symbols += "⇪"
+        }
+
+        return symbols
+    }
+}
+
 struct SettingsView: View {
     @AppStorage(
         Store.isTabsSwitcherNeededToStayOpenStoreKey,
         store: Store.userDefaults
     ) private var isTabsSwitcherNeededToStayOpen: Bool = false
     
+    @State
+    var shortcutModifiers =
+    KeyboardShortcuts.Name.openTabsList.shortcut?.modifiers.symbolRepresentation
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 120) {
             Toggle(isOn: !$isTabsSwitcherNeededToStayOpen) {
                 HStack {
-                    Text("Close tabs list when the shortcut is released")
+                    Text("Close tabs list when \(shortcutModifiers ?? "Option") is released")
                         .opacity(0.8)
                         .font(.system(size: 15))
                     Spacer()
@@ -37,7 +65,12 @@ struct SettingsView: View {
             .keyboardShortcut(.space, modifiers: [])
             .toggleStyle(.switch)
             
-            KeyboardShortcuts.Recorder(for: .openTabsList) {
+            KeyboardShortcuts.Recorder(
+                for: .openTabsList,
+                onChange: { shortcut in
+                    shortcutModifiers = shortcut?.modifiers.symbolRepresentation
+                }
+            ) {
                 HStack {
                     Text("Shortcut for opening tabs list")
                         .opacity(0.8)
