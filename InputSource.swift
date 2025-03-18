0272bcc4350754f78ed147533b726493ebd2fc92
kopyl <oleggggg1997@gmail.com>
2025-03-18 09:41:05 +0200

    Add ability to change language
    By pressing on a space key when there is no input

---
+++ InputSource.swift
@@ -0,0 +1,63 @@
+import InputMethodKit
+
+func getInputSourcesWithIDs() -> [(TISInputSource, String, Int)] {
+    let selectableIsProperties = [
+        kTISPropertyInputSourceType: kTISTypeKeyboardLayout as CFString,
+    ] as CFDictionary
+    
+    let inputSourceArray = TISCreateInputSourceList(selectableIsProperties, false).takeRetainedValue() as! [TISInputSource]
+    
+    return inputSourceArray.map { source in
+        let nameProperty = TISGetInputSourceProperty(source, kTISPropertyLocalizedName)
+        let name = nameProperty != nil ? Unmanaged<CFString>.fromOpaque(nameProperty!).takeUnretainedValue() as String : "Unknown"
+        
+        let description = String(describing: source)
+        var layoutID = -9999
+        
+        if let idRange = description.range(of: "id=") {
+            let afterIDRange = description[idRange.upperBound...]
+            if let endParenRange = afterIDRange.range(of: ")") {
+                let idString = description[idRange.upperBound..<endParenRange.lowerBound]
+                if let extractedID = Int(idString) {
+                    layoutID = extractedID
+                }
+            }
+        }
+        
+        return (source, name, layoutID)
+    }
+}
+
+func getInputSourcesSortedByHistory() -> [TISInputSource] {
+    guard let userDefaults = UserDefaults(suiteName: "com.apple.HIToolbox"),
+          let inputSourceHistory = userDefaults.array(forKey: "AppleInputSourceHistory") as? [[String: Any]] else {
+        return []
+    }
+    
+    let inputSourceArray = getInputSourcesWithIDs()
+    
+    var inputSourcesSortedByHistory: [TISInputSource] = []
+    
+    for item in inputSourceHistory {
+        guard let layoutID = item["KeyboardLayout ID"] else { return []}
+
+        for inputSource in inputSourceArray {
+            if inputSource.2 == layoutID as! Int {
+                inputSourcesSortedByHistory.append(inputSource.0)
+            }
+        }
+    }
+
+    return inputSourcesSortedByHistory
+}
+
+func changeInputSource(inputSource: TISInputSource) {
+    TISSelectInputSource(inputSource)
+}
+
+public func selectPreviousInputSource() {
+    let inputSources = getInputSourcesSortedByHistory()
+    guard inputSources.count > 1 else { return }
+    let prevInputSource = inputSources[1]
+    changeInputSource(inputSource: prevInputSource)
+}
