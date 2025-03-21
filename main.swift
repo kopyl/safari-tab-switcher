a730cd86c56c68ae7cbd5b59ad6ea25f472738c3
kopyl <oleggggg1997@gmail.com>
2025-03-21 06:24:02 +0200

    Add "About" window

--- main.swift
+++ main.swift
@@ -88,6 +88,7 @@ func handleHotKeyPress() {
 var greetingWindow: NSWindow?
 var tabsPanel: NSPanel?
 var settingsWindow: NSWindow?
+var aboutPanel: NSPanel?
 
 class AppState: ObservableObject {
     @Published var searchQuery = ""
@@ -249,6 +250,52 @@ func showSettingsWindow() {
     settingsWindow?.makeKeyAndOrderFront(nil)
 }
 
+struct AboutView: View {
+    var currentVersion: String {
+        Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            HStack(spacing: 12) {
+                Image(nsImage: NSApp.applicationIconImage)
+                    .resizable()
+                    .frame(width: 41, height: 41)
+                Text("Tab Finder")
+                    .font(.system(size: 28, weight: .regular))
+            }
+            Spacer()
+            HStack {
+                Text("Made by designer and developer [Oleh Kopyl](https://kopyloleh.com/)")
+                Spacer()
+                Text("\(currentVersion)")
+            }
+            .font(.system(size: 14))
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)   
+        .padding(.horizontal, 41)
+        .padding(.top, 33)
+        .padding(.bottom, 41)
+        .frame(minWidth: 444, minHeight: 240)
+    }
+}
+
+func showAboutPanel() {
+    if let aboutPanel {
+        aboutPanel.makeKeyAndOrderFront(nil)
+        return
+    }
+    
+    aboutPanel = Panel(
+        view: AboutView(),
+        styleMask: [.titled, .closable]
+    )
+    
+    aboutPanel?.setContentSize(NSSize(width: 444, height: 240))
+    aboutPanel?.center()
+    aboutPanel?.makeKeyAndOrderFront(nil)
+}
+
 class AppDelegate: NSObject, NSApplicationDelegate {
     var appState: AppState
     var panelObserver: TabsPanelVisibilityObserver?
@@ -285,6 +332,12 @@ class Application: NSApplication {
         self.mainMenu?.addItem(appMenuItem)
         let appMenu = NSMenu()
         appMenuItem.submenu = appMenu
+        
+        appMenu.addItem(NSMenuItem(title: "About \(ProcessInfo.processInfo.processName)",
+          action: #selector(openAboutPanel),
+          keyEquivalent: "")
+        )
+
         appMenu.addItem(NSMenuItem(title: "Quit \(ProcessInfo.processInfo.processName)",
            action: #selector(terminate(_:)),
            keyEquivalent: "q")
@@ -345,6 +398,10 @@ class Application: NSApplication {
     
     @objc func openSettingsWindow() {
         showSettingsWindow()
+    }
+
+    @objc func openAboutPanel() {
+        showAboutPanel()
     }
     
     @objc func openGreetingWindow() {
