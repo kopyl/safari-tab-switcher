kopyl/safari-tab-switcher

commit cf7f1a9584266bfeebc5af443f1fd57256837540

Make tabs panel react on appearance change when...
When it's still open and a user changed the appearance.
Before the appearance change used to properly react on system-wide appearance change only when tabs are completely re-rendered (the tabs panel is closed and opened again)

Tab Finder/Views/AppKit/TabHistoryView/Views/CloseButtonView.swift
@@ -11,7 +11,6 @@ final class CloseButtonView: NSView {
         self.tab = tab
         
         self.hoverBackgroundView = makeColorView()
-        self.hoverBackgroundView.layer?.backgroundColor = NSColor.lightGrey.cgColorAppearanceFix
         
         self.hoverBackgroundView.layer?.cornerRadius = 4
         
@@ -36,6 +35,10 @@ final class CloseButtonView: NSView {
             hoverBackgroundView.widthAnchor.constraint(equalToConstant: 20),
             hoverBackgroundView.heightAnchor.constraint(equalToConstant: 20),
         ])
+    }
+    
+    override func viewDidChangeEffectiveAppearance() {
+        self.hoverBackgroundView.layer?.backgroundColor = NSColor.lightGrey.cgColorAppearanceFix
     }
     
     override func mouseMoved(with event: NSEvent) {

Tab Finder/Views/AppKit/TabHistoryView/Views/Components/Favicons.swift
@@ -81,14 +81,19 @@ final class FaviconView: NSView {
         setupView()
     }
     
+    override func viewDidChangeEffectiveAppearance() {
+        textLayer.foregroundColor = NSColor.nearBlack.cgColorAppearanceFix
+        if imageLayer == nil {
+            layer?.backgroundColor = NSColor.lightGrey.cgColorAppearanceFix
+        }
+    }
+    
     private func setupView() {
         wantsLayer = true
-        layer?.backgroundColor = NSColor.lightGrey.cgColorAppearanceFix
         layer?.cornerRadius = 3
         
         textLayer.string = tab.host.first?.uppercased() ?? "N"
         textLayer.alignmentMode = .center
-        textLayer.foregroundColor = NSColor.nearBlack.cgColorAppearanceFix
         textLayer.font = NSFont.systemFont(ofSize: fontSize)
         textLayer.fontSize = fontSize
         textLayer.contentsScale = NSScreen.main?.backingScaleFactor ?? 2

Tab Finder/Views/AppKit/TabHistoryView/Views/Organisms.swift
@@ -107,10 +107,10 @@ func makeCloseButton() -> NSButton {
     return closeButton
 }
 
-func makeSwipeActionView(target: Any, action: Selector) -> NSView {
-    let buttonContainerView = NSView()
-    buttonContainerView.wantsLayer = true
-    buttonContainerView.layer?.backgroundColor = NSColor.customRed.cgColorAppearanceFix
+func makeSwipeActionView(target: Any, action: Selector) -> NSViewWithDynamicAppearance {
+    let buttonContainerView = NSViewWithDynamicAppearance() { view in
+        view.layer?.backgroundColor = NSColor.customRed.cgColorAppearanceFix
+    }
     buttonContainerView.layer?.cornerRadius = SwipeActionConfig.cornerRadius
     buttonContainerView.translatesAutoresizingMaskIntoConstraints = false
     
@@ -285,5 +285,26 @@ class TabsHeaderView: NSView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+final class NSViewWithDynamicAppearance: NSView {
+
+    private let updateHandler: (NSView) -> Void
+
+    init(frame: NSRect = .zero, onAppearanceChange: @escaping (NSView) -> Void) {
+        self.updateHandler = onAppearanceChange
+        super.init(frame: frame)
+        wantsLayer = true
+        updateHandler(self)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidChangeEffectiveAppearance() {
+        super.viewDidChangeEffectiveAppearance()
+        updateHandler(self)
     }
 }

Tab Finder/Views/AppKit/TabHistoryView/Views/TabHistoryView.swift
@@ -406,24 +406,8 @@ class TabHistoryView: NSViewController {
     }
     
     private func updateHighlighting() {
-        for (idx, tabView) in visibleTabViews {
-            if idx == appState.indexOfTabToSwitchTo {
-                if appState.renderedTabs[idx].id == -1 {
-                    tabView.contentView.layer?.backgroundColor = NSColor.currentClosedTabBg.cgColorAppearanceFix
-                }
-                else {
-                    tabView.contentView.layer?.backgroundColor = NSColor.currentOpenTabBg.cgColorAppearanceFix
-                }
-                
-                tabView.contentView.layer?.cornerRadius = 6
-                
-                tabView.firstColumnLabel.textColor = .currentTabFg
-                tabView.seconColumnLabel.textColor = .currentTabFg
-            } else {
-                tabView.contentView.layer?.backgroundColor = NSColor.clear.cgColorAppearanceFix
-                tabView.firstColumnLabel.textColor = .tabFg
-                tabView.seconColumnLabel.textColor = .tabFg
-            }
+        for (_, tabView) in visibleTabViews {
+            tabView.updateHighlighting()
         }
     }
     

Tab Finder/Views/AppKit/TabHistoryView/Views/TabItemView.swift
@@ -179,6 +179,11 @@ final class TabItemView: NSView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    override func viewDidChangeEffectiveAppearance() {
+        updateHighlighting()
+    }
+    
     override func mouseDown(with event: NSEvent) {
         appState.indexOfTabToSwitchTo = tab.renderIndex
         hideTabsPanelAndSwitchTabs()
@@ -291,6 +296,26 @@ final class TabItemView: NSView {
     override func mouseExited(with event: NSEvent) {
         closeButonView.isHidden = true
         isUserTryingToSwipeToCloseTab = false
+    }
+    
+    public func updateHighlighting() {
+        if tab.renderIndex == appState.indexOfTabToSwitchTo {
+            if tab.id == -1 {
+                contentView.layer?.backgroundColor = NSColor.currentClosedTabBg.cgColorAppearanceFix
+            }
+            else {
+                contentView.layer?.backgroundColor = NSColor.currentOpenTabBg.cgColorAppearanceFix
+            }
+            
+            contentView.layer?.cornerRadius = 6
+            firstColumnLabel.textColor = .currentTabFg
+            seconColumnLabel.textColor = .currentTabFg
+        }
+        else {
+            contentView.layer?.backgroundColor = NSColor.clear.cgColorAppearanceFix
+            firstColumnLabel.textColor = .tabFg
+            seconColumnLabel.textColor = .tabFg
+        }
     }
     
     private func setTotalSwipeDistance(event: NSEvent) {

commit ad6eb68741530803fab523f66ccb22dba7faf138

Fix hover of a tab close button
Broken in https://github.com/kopyl/safari-tab-switcher/commit/cf7f1a9584266bfeebc5af443f1fd57256837540

Tab Finder/Views/AppKit/TabHistoryView/Views/CloseButtonView.swift
@@ -11,6 +11,7 @@ final class CloseButtonView: NSView {
         self.tab = tab
         
         self.hoverBackgroundView = makeColorView()
+        self.hoverBackgroundView.layer?.backgroundColor = NSColor.lightGrey.cgColorAppearanceFix
         
         self.hoverBackgroundView.layer?.cornerRadius = 4
         